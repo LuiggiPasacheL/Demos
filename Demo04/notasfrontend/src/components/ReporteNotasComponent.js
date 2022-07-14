82439005d8803103d54e6952dae8b39f21932be8
Yanfer <oscar.yanfer@unmsm.edu.pe>
2022-07-14 12:29:15 -0500

    agregando demo 4

---
+++ Demo04/notasfrontend/src/components/ReporteNotasComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import CursoService from '../service/CursoService'
+
+class ReporteNotasComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            cursos: []
+        }
+    }
+
+    componentDidMount() {
+        CursoService.getCursos().then((res) => {
+            this.setState({ cursos: (res.data) })
+            console.log(res.data)
+        });
+    }
+
+    render() {
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th className="head" style={{color: "black"}}>Alumno</th>
+                        <th className="head" style={{color: "black"}}>Curso</th>
+                        <th className="head" style={{color: "black"}}>Promedio final</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.cursos.map(
+                            curso =>
+                                <tr key={curso.nombre}>
+                                    <td> {curso.alumnoID} </td>
+                                    <td> {curso.nombrecurso} </td>
+                                    <td> {curso.prom} </td>
+                                </tr>
+                        )
+                    }
+                </tbody>
+            </table >
+        )
+    }
+}
+
+export default ReporteNotasComponent;
